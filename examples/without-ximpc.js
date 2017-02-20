1ebb54af54bd778abc3802c6a704fa624e87035c
Sebastian Herrlinger <sebastian@formzoo.com>
2017-02-20 19:56:31 +0100

    share global workers

--- examples/without-ximpc.js
+++ examples/without-ximpc.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fibonacci = require('./fibonacci');
 const ximpc = require('../lib');
-const wrappedFibonacci = ximpc.wrapFn(path.resolve(__dirname, './fibonacci'));
+const wrappedFibonacci = ximpc.require(path.resolve(__dirname, './fibonacci'));
 
 const factor = 1000000000;
 let startTime = Date.now();
