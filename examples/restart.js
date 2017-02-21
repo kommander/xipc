84bec6455026d02b5450e860507fb977d78bdd31
Sebastian Herrlinger <sebastian@formzoo.com>
2017-02-21 01:38:40 +0100

    make workers restart and reconnect

---
+++ examples/restart.js
@@ -0,0 +1,31 @@
+const ximpc = require('../lib');
+const wrappedFibonacci = ximpc.require('./fns/fibonacci');
+
+const factor = 100000;
+let startTime = Date.now();
+const promises = [];
+
+for (let i = 0; i < 100; i++) {
+  promises.push(wrappedFibonacci(factor));
+}
+
+Promise.all(promises)
+  .then((results) => {
+    console.log('Num Results:', results.length);
+    const duration = Date.now() - startTime;
+    console.log('Duration XIMPC:', duration, 'ms');
+  })
+  .then(() => new Promise((resolve) => setTimeout(resolve, 4000)))
+  .then(() => {
+    const newPromises = [];
+    for (let i = 0; i < 100; i++) {
+      newPromises.push(wrappedFibonacci(factor));
+    }
+    startTime = Date.now();
+    return Promise.all(newPromises);
+  })
+  .then((results) => {
+    console.log('Num Results:', results.length);
+    const duration = Date.now() - startTime;
+    console.log('Duration XIMPC:', duration, 'ms');
+  });
